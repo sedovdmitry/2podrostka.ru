63265525ce40d8a57f12870449bd60a42cc0c24b
Dmitry Sedov <sedov.dmitry1@gmail.com>
2025-10-12 18:45:24 +0300

    GTM

--- parts/analytics.php
+++ parts/analytics.php
@@ -11,17 +11,19 @@
 </script>
 <noscript><div><img src="https://mc.yandex.ru/watch/23378290" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
-<!-- Google Analytics -->
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  
-  ga('create', 'UA-52710716-1', 'auto');
-  ga('send', 'pageview', 'event', 'button', 'click', 'nav buttons', 4);
-</script>
-<!-- GoogleEnd -->
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TBDTHJHF');</script>
+<!-- End Google Tag Manager -->
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TBDTHJHF"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
 <!-- Top.Mail.Ru counter -->
 <script type="text/javascript">
 var _tmr = window._tmr || (window._tmr = []);
--- parts/footer-scripts.php
+++ parts/footer-scripts.php
@@ -2,7 +2,23 @@
 <script src="/js/jquery.1.8.0.min.js"></script>
 <!-- JavaScript -->
 <script async src="./gzip-js/default.js"></script>
-<script>
-    // Асинхронный код GA, через 10 секунд отправляет в GA сигнал, что страница еще открыта
-    setTimeout("ga('send','event','Engaged users','More than 10 seconds')", 10000);
+
+<script type="module">
+  import {getLCP, getINP, getCLS, getFCP, getTTFB} from 'https://unpkg.com/web-vitals?module';
+
+  function sendToGoogleAnalytics({name, value, delta}) {
+    const roundedValue = Math.round(name === 'CLS' ? value * 1000 : value);  // CLS в миллисекундах
+    gtag('event', name, {
+      event_category: 'Web Vitals',
+      value: roundedValue,
+      event_label: name,
+      non_interaction: true
+    });
+  }
+
+  getLCP(sendToGoogleAnalytics);
+  getINP(sendToGoogleAnalytics);
+  getCLS(sendToGoogleAnalytics);
+  getFCP(sendToGoogleAnalytics);  // Опционально
+  getTTFB(sendToGoogleAnalytics);  // Опционально
 </script>
