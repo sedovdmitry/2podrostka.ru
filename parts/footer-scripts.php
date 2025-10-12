8e71352f58d569495175ab81023f4e20b88653e1
Dmitry Sedov <sedov.dmitry1@gmail.com>
2025-10-12 19:19:35 +0300

    Core Web Vitals fix

--- parts/footer-scripts.php
+++ parts/footer-scripts.php
@@ -2,23 +2,31 @@
 <script src="/js/jquery.1.8.0.min.js"></script>
 <!-- JavaScript -->
 <script async src="./gzip-js/default.js"></script>
+<script>
+  (function() {
+    var script = document.createElement('script');
+    script.src = 'https://unpkg.com/web-vitals@3/dist/web-vitals.iife.js';
+    script.async = true;
+    script.onload = function() {
+      webVitals.onCLS(function(metric) { sendToGoogleAnalytics(metric); });
+      webVitals.onLCP(function(metric) { sendToGoogleAnalytics(metric); });
+      webVitals.onINP(function(metric) { sendToGoogleAnalytics(metric); });
+      webVitals.onFCP(function(metric) { sendToGoogleAnalytics(metric); });
+      webVitals.onTTFB(function(metric) { sendToGoogleAnalytics(metric); });
+    };
+    document.head.appendChild(script);
 
-<script type="module">
-  import {getLCP, getINP, getCLS, getFCP, getTTFB} from 'https://unpkg.com/web-vitals?module';
-
-  function sendToGoogleAnalytics({name, value, delta}) {
-    const roundedValue = Math.round(name === 'CLS' ? value * 1000 : value);  // CLS в миллисекундах
-    gtag('event', name, {
-      event_category: 'Web Vitals',
-      value: roundedValue,
-      event_label: name,
-      non_interaction: true
-    });
-  }
-
-  getLCP(sendToGoogleAnalytics);
-  getINP(sendToGoogleAnalytics);
-  getCLS(sendToGoogleAnalytics);
-  getFCP(sendToGoogleAnalytics);  // Опционально
-  getTTFB(sendToGoogleAnalytics);  // Опционально
+    function sendToGoogleAnalytics({name, value, delta}) {
+      var roundedValue = Math.round(name === 'CLS' ? value * 1000 : value);
+      window.dataLayer = window.dataLayer || [];
+      window.dataLayer.push({
+        event: 'web_vitals',
+        event_category: 'Web Vitals',
+        event_action: name,
+        event_value: roundedValue,
+        event_label: name,
+        non_interaction: true
+      });
+    }
+  })();
 </script>
