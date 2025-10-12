44c939f099665c363a0d3abf2e3861120e9d2596
Dmitry Sedov <sedov.dmitry1@gmail.com>
2025-10-12 19:13:36 +0300

    prettier + eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "jquery": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "script"
+  },
+  "rules": {
+    "no-var": "error",
+    "prefer-const": "error"
+  }
+};
